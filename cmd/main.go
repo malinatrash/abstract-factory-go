768fc4f1ebd10650f8ea36c41637b0ac13c3c8c7
Pavel Naumov <malinatrashh@gmail.com>
2024-02-25 13:10:51 +0800

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,7 @@ func main() {
 
 	for factory == nil {
 		var choice int
+		fmt.Println("---------------------–––––")
 		fmt.Println("Выберите линейку костюмов:")
 		for index, brand := range brands {
 			fmt.Printf("%d. %s\n", index+1, brand)
@@ -25,6 +26,8 @@ func main() {
 			if err != nil {
 				fmt.Printf("%s\n", err)
 			}
+		} else {
+			fmt.Println("Ошибка")
 		}
 	}
 
